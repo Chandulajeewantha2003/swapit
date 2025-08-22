03ee7b97cd1fa6cfa24e4162d99e6950abc0791a
Chandulajeewantha2003 <chandulajeewantha2003@gmail.com>
2025-08-22 18:35:04 +0530

    main activity 4 kt

--- app/src/main/java/com/example/swapit/MainActivity4.kt
+++ app/src/main/java/com/example/swapit/MainActivity4.kt
@@ -1,6 +1,8 @@
 package com.example.swapit
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class MainActivity4 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main4)
+
+        // Apply system insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Get Started button → redirect to MainActivity5
+        val getStartedBtn = findViewById<Button>(R.id.button2)
+        getStartedBtn.setOnClickListener {
+            //val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
         }
     }
 }
