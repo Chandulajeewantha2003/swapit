c6574068755e4bffda50ed023ca5a4737e3eaa9f
Chandulajeewantha2003 <chandulajeewantha2003@gmail.com>
2025-08-22 18:24:39 +0530

    main activity kt

--- app/src/main/java/com/example/swapit/MainActivity.kt
+++ app/src/main/java/com/example/swapit/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.swapit
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 5000 // 5000ms = 5s
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Adjust window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Auto redirect after 5 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish() // close splash so user can't return with back button
+        }, SPLASH_TIME)
     }
 }
